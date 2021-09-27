ebca19cf072e83abfbb690cbb4c91ec10fa353e1
Yulei Zhu <yulei.zhu@movember.com>
2021-09-27 16:24:56 +1000

    feat: set up index for sort

--- src/algorithms/sort/index.ts
+++ src/algorithms/sort/index.ts
@@ -3,7 +3,7 @@ import Selection from './selectionSort'
 import Insertion from './insertionSort'
 
 export default () => {
-  const SIZE = 100
+  const SIZE = 10000
   const input: number[] = []
 
   const random = () => {
@@ -13,17 +13,45 @@ export default () => {
   }
 
   random()
+
   const p1 = performance.now()
-
   Selection.sort(input)
-
   const p2 = performance.now()
+
+  const s1 = Selection.isSorted(input)
+
   random()
 
   const p3 = performance.now()
-
   Insertion.sort(input)
-  console.log(Insertion.isSorted(input))
   const p4 = performance.now()
+  const s2 = Insertion.isSorted(input)
+
   random()
+
+  const p5 = performance.now()
+  input.sort((a, b) => a - b)
+  const p6 = performance.now()
+  const s3 = Insertion.isSorted(input)
+
+  console.warn(`[sort] Testing for ${SIZE} cases...`)
+
+  console.log(
+    `Selection sort ${
+      s1 ? `took ${Math.round((p2 - p1) * 100) / 100} ms` : 'failed...'
+    }`
+  )
+
+  console.log(
+    `Insertion sort ${
+      s2 ? `took ${Math.round((p4 - p3) * 100) / 100} ms` : 'failed...'
+    }`
+  )
+  console.log(
+    `Default sort ${
+      s3 ? `took ${Math.round((p6 - p5) * 100) / 100} ms` : 'failed...'
+    }`
+  )
+
+  console.warn('[sort] Testing finished.')
 }
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import unionFind from './algorithms/unionFind'
 import sort from './algorithms/sort'
+
 createApp(App).mount('#app')
 
 unionFind()
-
+console.log('----------------------------------------------------------------')
 sort()
+console.log('----------------------------------------------------------------')
