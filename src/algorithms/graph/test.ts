a699fb0408545f5af5d8839a9fb933aed24d0f3e
Yulei Zhu <zhuyulei12333@gmail.com>
2021-10-19 10:02:55 +1100

    refactor: rename graph

---
+++ src/algorithms/graph/test.ts
@@ -0,0 +1,105 @@
+/**
+ * Class of undirected graph
+ */
+export default class Graph {
+  /**
+   * Constructor of an undirected graph
+   *
+   * @param  {array} input Array representing the graph
+   */
+  constructor(input: number[]) {
+    this.V = input.shift() ?? 0
+    this.E = input.shift() ?? 0
+    for (let i = 0; i < input.length; i += 2) {
+      this.addEdge(i, i + 1)
+    }
+  }
+
+  /**
+   * Number of vertices
+   */
+  V: number
+
+  /**
+   * Number of edges
+   */
+  E: number
+
+  /**
+   * Table of adjacent vertices of each vertex
+   */
+  adj: number[][] = []
+
+  /**
+   * Add an edge between two vertices
+   *
+   * @param  {number} v vertex 1
+   * @param  {number} w vertex 2
+   */
+  addEdge = (v: number, w: number) => {
+    this.adj[v].push(w)
+    this.adj[w].push(v)
+    this.E++
+  }
+
+  /**
+   * Get all adjacent vertices of a given vertex
+   *
+   * @param  {number} v
+   * @returns array of vertices
+   */
+  getAdj = (v: number): number[] => {
+    return this.adj[v]
+  }
+
+  /**
+   * Get the degree of a vertex
+   *
+   * Where degree is the number of edges it is connected to
+   *
+   * @param  {number} v vertex
+   * @returns degree
+   */
+  degree(v: number): number {
+    return this.getAdj(v).length
+  }
+
+  /**
+   * Get the maximum degree of the graph
+   *
+   * Where degree is the number of edges it is connected to
+   *
+   * @returns degree
+   */
+  maxDegree(): number {
+    let max = 0
+    for (let v = 0; v < this.V; v++) {
+      if (this.degree(v) > max) {
+        max = this.degree(v)
+      }
+    }
+    return max
+  }
+
+  /**
+   * Get the average degree of the graph
+   *
+   * Where degree is the number of edges it is connected to
+   *
+   * @returns degree
+   */
+  avgDegree(): number {
+    return (2 * this.E) / this.V
+  }
+
+  numberOfSelfLoops(): number {
+    let count = 0
+    for (let v = 0; v < this.V; v++) {
+      for (let w of this.getAdj(v)) {
+        if (v === w) count++
+      }
+    }
+    // Each edge is being counted twice
+    return count / 2
+  }
+}
