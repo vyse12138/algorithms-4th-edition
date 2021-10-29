b7ea19dc3cfc23675f7826301da674916768d5b3
Yulei Zhu <zhuyulei12333@gmail.com>
2021-10-29 20:30:21 +1100

    feat(graph): shortest path

---
+++ src/algorithms/graph/edgeWeightedDiGraph/dijkstraSP.ts
@@ -0,0 +1,53 @@
+import Graph from './edgeWeightedDiGraph'
+import Edge from './diEdge'
+export default class SP {
+  constructor(g: Graph, s: number) {
+    this.distTo = new Array(g.V).fill(Number.MAX_SAFE_INTEGER)
+
+    this.distTo[s] = 0
+  }
+
+  pathTo: Edge[] = []
+  distTo: number[] = []
+  edgeTo: Edge[] = []
+
+  getDistTo = (v: number): number => {
+    return this.distTo[v]
+  }
+
+  hasPathTo = (v: number): boolean => {
+    return this.distTo[v] < Number.MAX_SAFE_INTEGER
+  }
+
+  pathToDistTo = (v: number): Edge[] => {
+    if (!this.hasPathTo(v)) return []
+
+    let path = []
+
+    for (let e = this.edgeTo[v]; e != null; e = this.edgeTo[e.from]) {
+      path.push(e)
+    }
+    return path
+  }
+
+  private relaxEdge = (e: Edge) => {
+    let v = e.from,
+      w = e.to
+
+    if (this.distTo[w] > this.distTo[v] + e.weight) {
+      this.distTo[w] = this.distTo[v] + e.weight
+      this.edgeTo[w] = e
+    }
+  }
+
+  private relaxVertex = (g: Graph, v: number) => {
+    for (let e of g.getAdj(v)) {
+      let w = e.to
+
+      if (this.distTo[w] > this.distTo[v] + e.weight) {
+        this.distTo[w] = this.distTo[v] + e.weight
+        this.edgeTo[w] = e
+      }
+    }
+  }
+}
