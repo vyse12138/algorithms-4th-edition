6c0e948eaf9f8ad0ce265fb0359e2e237500a7a6
Yulei Zhu <zhuyulei12333@gmail.com>
2021-11-08 14:59:05 +1100

    feat(string): string ST

---
+++ src/algorithms/string/stringST.ts
@@ -0,0 +1,83 @@
+type Value = number | null
+
+class Node {
+  val: Value = 0
+  next: Node[] = new Array(256).fill(new Node())
+}
+
+// String search tree
+export default class StringST {
+  private R = 256
+  root: Node = new Node()
+
+  get = (key: string): Value => {
+    let x: Node | null = this.getR(this.root, key, 0)
+
+    if (x === null) return null
+
+    return x.val
+  }
+
+  getR = (x: Node, key: string, d: number): Node | null => {
+    if (x === null) {
+      return null
+    }
+
+    if (d === key.length) {
+      return x
+    }
+
+    let c = key.charCodeAt(d)
+
+    return this.getR(x.next[c], key, d + 1)
+  }
+
+  put = (key: string, val: Value) => {
+    this.root = this.putR(this.root, key, val, 0)
+  }
+
+  putR = (x: Node | null, key: string, val: Value, d: number) => {
+    if (x === null) {
+      x = new Node()
+    }
+
+    if (d === key.length) {
+      x.val = val
+      return x
+    }
+
+    let c = key.charCodeAt(d)
+
+    x.next[c] = this.putR(x.next[c], key, val, d + 1)
+
+    return x
+  }
+
+  delete = (key: string) => {}
+
+  contains = (key: string): boolean => {
+    return false
+  }
+
+  isEmpty = (): boolean => {
+    return false
+  }
+
+  longestPrefixOf = (s: string): string => {
+    return ''
+  }
+
+  keysWithPrefix = (s: string): string[] => {
+    return []
+  }
+
+  /**
+   * `.` can match anything
+   *
+   * @param  {string} s
+   * @returns string[]
+   */
+  keysThatMatch = (s: string): string[] => {
+    return []
+  }
+}
