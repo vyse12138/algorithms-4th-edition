56a393002996a116f2d5dc412ec156c57ae1f796
Yulei Zhu <zhuyulei12333@gmail.com>
2021-11-09 14:49:09 +1100

    refactor(string): add index

---
+++ src/algorithms/string/radixSort/index.ts
@@ -0,0 +1,67 @@
+import keyIndexCounting from './keyIndexCounting'
+import LSD from './radixSortLSD'
+import MSD from './radixSortMSD'
+
+export default () => {
+  const SIZE = 50000
+  const LENGTH = 8
+
+  console.warn(`[string] Testing for ${SIZE} cases...`)
+
+  // char code for a-z is 97-122
+  let s = ''
+  for (let i = 0; i < SIZE; i++) {
+    let char = String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+    s = s.concat(char)
+  }
+
+  let p1 = performance.now()
+  keyIndexCounting(s)
+  let p2 = performance.now()
+
+  let a = []
+  for (let i = 0; i < SIZE; i++) {
+    let temp = ''
+    for (let j = 0; j < LENGTH; j++) {
+      let char = String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+      temp = temp.concat(char)
+    }
+    a.push(temp)
+  }
+
+  let p3 = performance.now()
+  LSD.sort(a, LENGTH)
+  let p4 = performance.now()
+
+  a = []
+  for (let i = 0; i < SIZE; i++) {
+    let temp = ''
+    for (let j = 0; j < Math.ceil(Math.random() * 8); j++) {
+      let char = String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+      temp = temp.concat(char)
+    }
+    a.push(temp)
+  }
+
+  let p5 = performance.now()
+  MSD.sort(a, LENGTH)
+  let p6 = performance.now()
+
+  console.log(
+    `Key index counting took ${
+      Math.round((p2 - p1) * 100) / 100
+    } ms for string with ${SIZE} chars`
+  )
+  console.log(
+    `LSD took ${
+      Math.round((p4 - p3) * 100) / 100
+    } ms for ${SIZE} strings with ${LENGTH} chars`
+  )
+  console.log(
+    `MSD took ${
+      Math.round((p6 - p5) * 100) / 100
+    } ms for ${SIZE} strings with 0 to ${LENGTH} chars`
+  )
+
+  console.warn(`[string] Testing finished.`)
+}
